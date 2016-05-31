42d4d78066b89fd6e791ee8ad14c3ecaef715fc7
Ivan Dovgan <ivandov@us.ibm.com>
2016-05-31 14:12:33 -0400

    Modified docker setup script to support installation in IBM Container environment

--- scripts/share/setup_docker.sh
+++ scripts/share/setup_docker.sh
@@ -15,16 +15,44 @@ cd ../..
 rm -r docker-compose
 }
 
+install_docker_ubuntu ()
+{
+	#start docker-compose install procedure as documented on https://docs.docker.com/engine/installation/linux/ubuntulinux/
+	apt-get update
+	apt-get install apt-transport-https ca-certificates
+	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+	rm -f /etc/apt/sources.list.d/docker.list
+	touch /etc/apt/sources.list.d/docker.list
+	echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+	apt-get update
+	apt-get purge lxc-docker
+	apt-cache policy docker-engine
+
+	#pre-requisites for Ubuntu 14.04 (IBM containers are built on this)
+	apt-get install linux-image-extra-$(uname -r)
+	apt-get install apparmor
+
+	#install
+	apt-get update
+	apt-get install docker-engine
+	service docker start
+	docker run hello-world
+}
+
 # Checking if docker daemon is running
 DOCKER_RUNNING=`ps aux | grep -v grep | grep 'docker daemon'`
-if [ "${DOCKER_RUNNING:-null}" = null ] ; then 
+if [ "${DOCKER_RUNNING:-null}" = null ] ; then
 	echo "Docker is not running..."
-	if [ ! -x /usr/bin/docker  ] ; then
+	if [[$(uname -a) == *"Ubuntu"*]] then
+		echo "Detected Ubuntu Operating System"
+		install_docker_ubuntu
+	elif [ ! -x /usr/bin/docker  ] ; then
 		wget ftp://ftp.unicamp.br/pub/linuxpatch/s390x/suse/sles12/docker/docker-1.9.1-sles12-20151127.tar.gz
 		tar -xvzf docker-1.9.1-sles12-20151127.tar.gz
 		cp ./docker-1.9.1-sles12-20151127/docker /usr/bin
 	fi
-	docker daemon -g ${DOCKER_DAEMON_DIRECTORY} --insecure-registry ${REGISTRY}:${REGISTRY_PORT} -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock &
+	#docker daemon -g ${DOCKER_DAEMON_DIRECTORY} --insecure-registry ${REGISTRY}:${REGISTRY_PORT} -H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock &
+	docker daemon -g ${DOCKER_DAEMON_DIRECTORY}
 	disown
 else
 	echo "Good! Docker daemon is running."
@@ -33,7 +61,7 @@ fi
 # Checking if docker-compose is installed
 if [ ! -x /usr/bin/docker-compose ] ; then
 	echo "Docker-compose is not installed, install now..."
-	install_docker_compose
+	#install_docker_compose
 else
 	echo "Good! Docker-compose is installed."
-fi 
+fi
