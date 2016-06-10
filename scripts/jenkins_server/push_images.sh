e4b91b8726fcad814b0563c79d286eaeab78e647
Ivan Dovgan <ivandov@us.ibm.com>
2016-06-10 10:17:01 -0400

    updated push_images script to include all ubuntu images

--- scripts/jenkins_server/push_images.sh
+++ scripts/jenkins_server/push_images.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-read -r -a IMGS <<< $(echo $(docker images | awk '{print $1}' | grep jenkins))
+read -r -a IMGS <<< $(echo $(docker images | awk '{print $1}' | grep -v 's390x/ubuntu' | grep ubuntu))
 #docker tag ${IMGS[1]} "lal/${IMGS[1]}"
 for img in "${IMGS[@]}"
 do
 	echo "$img"
-	docker tag $img "${REGISTRY}:${REGISTRY_PORT}/${USR_NAME}/bluemix_$img"
-	docker push "${REGISTRY}:${REGISTRY_PORT}/${USR_NAME}/bluemix_$img"
-	docker rmi "${REGISTRY}:${REGISTRY_PORT}/${USR_NAME}/bluemix_$img"
+	docker tag $img "${USR_NAME}/s390x_$img"
+	docker push "${USR_NAME}/s390x_$img"
+	docker rmi "${USR_NAME}/s390x_$img"
 done
