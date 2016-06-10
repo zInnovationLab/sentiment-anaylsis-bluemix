8919ae24c9ff9501e48c6e790e0335283eb20d75
Ivan Dovgan <ivandov@us.ibm.com>
2016-06-10 14:14:48 -0400

    updated pull_image script to remove extra info returned by docker search

--- scripts/jenkins_slave/pull_images.sh
+++ scripts/jenkins_slave/pull_images.sh
@@ -2,7 +2,7 @@
 
 #read -r -a IMGS <<< $(echo $(curl http://${REGISTRY}:${REGISTRY_PORT}/v2/_catalog |python -m json.tool | grep -o '"'${USR_NAME}'/bluemix.*"' | sed 's/"//g'))
 
-read -r -a IMGS <<< $(echo $(docker search ${USR_NAME}/s390x))
+read -r -a IMGS <<< $(echo $(docker search ivandov/s390x | awk '{print $1}' | grep -v NAME))
 
 for img in "${IMGS[@]}"
 do
