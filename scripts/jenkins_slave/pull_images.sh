959a3c7c8dbaf11c200afc4acf4e124cd8c75a6e
Ivan Dovgan <ivandov@us.ibm.com>
2016-06-10 11:29:23 -0400

    updated pull_image script to pull from public docker hub

--- scripts/jenkins_slave/pull_images.sh
+++ scripts/jenkins_slave/pull_images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-read -r -a IMGS <<< $(echo $(curl http://${REGISTRY}:${REGISTRY_PORT}/v2/_catalog |python -m json.tool | grep -o '"'${USR_NAME}'/bluemix.*"' | sed 's/"//g'))
+#read -r -a IMGS <<< $(echo $(curl http://${REGISTRY}:${REGISTRY_PORT}/v2/_catalog |python -m json.tool | grep -o '"'${USR_NAME}'/bluemix.*"' | sed 's/"//g'))
+
+read -r -a IMGS <<< $(echo $(docker search ${USR_NAME}/s390x))
 
 for img in "${IMGS[@]}"
 do
