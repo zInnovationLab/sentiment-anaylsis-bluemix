a7e82789a2f966c12a5f2bb09b7565069e7d6988
Ivan Dovgan <ivandov@us.ibm.com>
2016-06-17 13:17:09 -0400

    added hooks for bluemix and docker compose

--- scripts/jenkins_server/ubuntu_x86/push_images.sh
+++ scripts/jenkins_server/ubuntu_x86/push_images.sh
@@ -8,4 +8,13 @@ do
 	docker tag $img "${USR_NAME}/x86_$img"
 	docker push "${USR_NAME}/x86_$img"
 	docker rmi "${USR_NAME}/x86_$img"
+
+	cf ic cpi "${USR_NAME}/x86_$img" "registry.ng.bluemix.net/ivandov/x86_$img:latest"
 done
+
+# VARIABLES NEEDED TO BE SET TO RUN CF IC COMMAND AS DOCKER
+export DOCKER_HOST=tcp://containers-api.ng.bluemix.net:8443
+export DOCKER_CERT_PATH=/home/ivandov/.ice/certs/containers-api.ng.bluemix.net/b8f3e99e-8740-4afa-bf06-c740ad05ea82
+export DOCKER_TLS_VERIFY=1
+
+docker-compose up
